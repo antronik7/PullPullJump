96f17c26b179bb4a96193e0ba75879371e343825
AntoineDutil <antoineberthiaumedutil@gmail.com>
2017-05-03 23:57:25 -0400

    Commencer les controlles tactiles.

---
+++ PullPullJump/Assets/Scripts/ControllerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerController : MonoBehaviour {
+
+    [SerializeField]
+    private float rangePull = 1.5f;
+
+    [SerializeField]
+    private GameObject smallController;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 allowedPos = objectPosition - transform.position;
+
+        allowedPos = Vector3.ClampMagnitude(allowedPos, rangePull);
+
+        smallController.transform.position = transform.position + allowedPos;
+    }
+}
---
+++ PullPullJump/Assets/Scripts/GameplayController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform spriteScale;
+    [SerializeField]
+    private GameObject controller;
+
+    private GameObject myController;
+
+    void Awake()
+    {
+        myController = Instantiate(controller, transform.position, Quaternion.identity);
+        myController.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            activateController();
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            desactivateController();
+        }
+    }
+
+    void activateController()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        objectPos.z = 0f;
+
+        myController.transform.position = objectPos;
+        myController.transform.parent = Camera.main.transform;
+
+        myController.SetActive(true);
+    }
+
+    void desactivateController()
+    {
+        myController.SetActive(false);
+    }
+}
